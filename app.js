d443c7eee475baa08a072bf8a089556e19b57aa5
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-07-01 14:35:07 +0800

    try commit in repo

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const morgan = require("morgan")
+
+const jwt = require("jsonwebtoken")
 
 const app = express()
+
+app.use(morgan("dev"))
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to mongodb"))
+
+const PORT = 3333
+app.listen(PORT, () => console.log("Server listening to"))
